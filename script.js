18d004ae00c3d0f1624eb7f5dcf1d6d1dcc84ead
PBuinizki <61580868+PBuinizki@users.noreply.github.com>
2022-10-30 20:52:11 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-"use strict"
+$('.slick-slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    centerMode: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToScroll: 1,
+    mobileFirst: true,
+    responsive: [
+        {
+            breakpoint: 1024,
+             settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    autoplay: false,
+                }
+        }
+    ]
+  });
